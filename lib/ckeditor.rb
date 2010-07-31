32f84e02e088c72adb82ff466f3b3ef0b6846da3
atalargo <atalargo@free.fr>
2010-07-31 13:43:31 +0200

    Add id and name options for input/textarea available

--- lib/ckeditor.rb
+++ lib/ckeditor.rb
@@ -2,14 +2,14 @@
 module Ckeditor
   PLUGIN_NAME = 'rails-ckeditor'
   PLUGIN_PATH = File.join(RAILS_ROOT, "vendor/plugins", PLUGIN_NAME)
-  
+
   PLUGIN_PUBLIC_PATH = Ckeditor::Config.exists? ? Ckeditor::Config['public_path'] : "#{RAILS_ROOT}/public/uploads"
   PLUGIN_PUBLIC_URI  = Ckeditor::Config.exists? ? Ckeditor::Config['public_uri'] : "/uploads"
-  
+
   PLUGIN_CONTROLLER_PATH = File.join(PLUGIN_PATH, "/app/controllers")
   PLUGIN_VIEWS_PATH      = File.join(PLUGIN_PATH, "/app/views")
   PLUGIN_HELPER_PATH     = File.join(PLUGIN_PATH, "/app/helpers")
-  
+
   PLUGIN_FILE_MANAGER_URI        = Ckeditor::Config.exists? ? Ckeditor::Config['file_manager_uri'] : ""
   PLUGIN_FILE_MANAGER_UPLOAD_URI = Ckeditor::Config.exists? ? Ckeditor::Config['file_manager_upload_uri'] : ""
   PLUGIN_FILE_MANAGER_IMAGE_URI  = Ckeditor::Config.exists? ? Ckeditor::Config['file_manager_image_uri'] : ""
@@ -17,13 +17,13 @@ module Ckeditor
 
   module Helper
     include ActionView::Helpers
-    
+
     def ckeditor_textarea(object, field, options = {})
       options.symbolize_keys!
-      
+
       var = options.delete(:object) if options.key?(:object)
       var ||= @template.instance_variable_get("@#{object}")
-      
+
       if var
         value = var.send(field.to_sym)
         value = value.nil? ? "" : value
@@ -32,7 +32,12 @@ module Ckeditor
         klass = "#{object}".camelcase.constantize
         instance_variable_set("@#{object}", eval("#{klass}.new()"))
       end
-      id = ckeditor_element_id(object, field)
+
+      id = if options[:id].nil?
+		ckeditor_element_id(object, field)
+	   else
+		ckeditor_element_id(options[:id])
+	   end
 
       cols = options[:cols].nil? ? "cols='70'" : "cols='"+options[:cols]+"'"
       rows = options[:rows].nil? ? "rows='20'" : "rows='"+options[:rows]+"'"
@@ -41,31 +46,37 @@ module Ckeditor
       height = options[:height].nil? ? '100%' : options[:height]
 
       classy = options[:class].nil? ? '' : "class='#{options[:class]}'"
-      
+
       ckeditor_options = {}
-      
+
       ckeditor_options[:toolbar] = options[:toolbar] unless options[:toolbar].nil?
       ckeditor_options[:skin] = options[:skin] unless options[:skin].nil?
       ckeditor_options[:language] = options[:language] unless options[:language].nil?
       ckeditor_options[:width] = options[:width] unless options[:width].nil?
       ckeditor_options[:height] = options[:height] unless options[:height].nil?
-      
+
       ckeditor_options[:swf_params] = options[:swf_params] unless options[:swf_params].nil?
-      
+
       ckeditor_options[:filebrowserBrowseUrl] = PLUGIN_FILE_MANAGER_URI
       ckeditor_options[:filebrowserUploadUrl] = PLUGIN_FILE_MANAGER_UPLOAD_URI
-      
+
       ckeditor_options[:filebrowserImageBrowseUrl] = PLUGIN_FILE_MANAGER_IMAGE_URI
       ckeditor_options[:filebrowserImageUploadUrl] = PLUGIN_FILE_MANAGER_IMAGE_UPLOAD_URI
 
+
+	name_tag = if options[:name].nil?
+			"#{object}[#{field}]"
+		   else
+			options[:name]
+		   end
       if options[:ajax]
-        inputs = "<input type='hidden' id='#{id}_hidden' name='#{object}[#{field}]'>\n" <<
+        inputs = "<input type='hidden' id='#{id}_hidden' name='#{name_tag}'>\n" <<
                  "<textarea id='#{id}' #{cols} #{rows} name='#{id}'>#{value}</textarea>\n"
       else
-        inputs = "<textarea id='#{id}' style='width:#{width};height:#{height}' #{cols} #{rows} #{classy} name='#{object}[#{field}]'>#{h value}</textarea>\n"
+        inputs = "<textarea id='#{id}' style='width:#{width};height:#{height}' #{cols} #{rows} #{classy} name='#{name_tag}'>#{h value}</textarea>\n"
       end
-      
-      return inputs << javascript_tag("CKEDITOR.replace('#{object}[#{field}]', { 
+
+      return inputs << javascript_tag("CKEDITOR.replace('#{id}', {
           #{ckeditor_applay_options(ckeditor_options)}
         });\n")
     end
@@ -90,10 +101,12 @@ module Ckeditor
     end
     alias_method :ckeditor_form_remote_for, :ckeditor_remote_form_for
 
-    def ckeditor_element_id(object, field)
-      #id = eval("@#{object}.id")
-      #"#{object}_#{id}_#{field}_editor"
-      "#{object}_#{field}_editor"
+    def ckeditor_element_id(object, field = nil)
+	if field.nil?
+		"#{object}_editor"
+	else
+		"#{object}_#{field}_editor"
+	end
     end
 
     def ckeditor_div_id(object, field)
@@ -105,7 +118,7 @@ module Ckeditor
       id = ckeditor_element_id(object, field)
       "var oEditor = CKEDITOR.instances.#{id}.getData();"
     end
-    
+
     def ckeditor_applay_options(options={})
       str = []
       options.each do |k, v|
@@ -116,7 +129,7 @@ module Ckeditor
         end
         str << "#{k}: #{value}"
       end
-      
+
       str.join(',')
     end
   end
@@ -144,7 +157,7 @@ end
 module ActionView::Helpers
 	class FormBuilder
 		include Ckeditor::Helper
-		
+
 		def cktext_area(method, options = {})
     	ckeditor_textarea(@object_name, method, objectify_options(options))
     end
